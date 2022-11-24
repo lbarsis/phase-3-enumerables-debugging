4a0dba62fbefdd0858215183adc49aff17aa3dc4
Levi Barsis <lbarsis@gmail.com>
2022-11-24 11:00:52 -0800

    Created function

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,12 @@
 # Write your code here
+require 'pry'
+
+# 1. Split string on " " to get access to each word
+# 2. Reverse each word, and add it to a new array
+# 3. Join the array of words on " " to create one string
+
+def reverse_each_word(str)
+  reversed_words = str.split.collect(&:reverse).join(" ")
+end
+
+binding.pry
